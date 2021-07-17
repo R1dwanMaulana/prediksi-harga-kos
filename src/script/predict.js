ad2b5902c8891dbb40f289b07248c0822196991a
Ridwan Maulana <rdwanf49@gmail.com>
2021-07-17 17:43:56 +0700

    update project

--- src/script/predict.js
+++ src/script/predict.js
@@ -4,6 +4,9 @@ document.getElementById("tombol_form").
     let panjang=document.getElementById("panjang").value;
     let lebar=document.getElementById("lebar").value;
     let kamar_mandi_VIP=document.getElementById("kamar_mandi_VIP").value;
+    // if(document.getElementById.checked) {
+    //   kamar_mandi_VIP = kamar_mandi_VIP + checkbox[i].value;
+    // }
     let kamar_mandi=document.getElementById("kamar_mandi").value;
     let predik = -238586.45 + (1.90488666e+05 * panjang) + (1.85814320e+02 * lebar) + (1.08816474e+04 * kamar_mandi_VIP) + (2.93902995e+03 * kamar_mandi);
     document.getElementById("hasil").innerHTML=parseInt(predik);
